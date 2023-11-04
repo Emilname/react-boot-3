82777a34144b5503e34e4995741fb7d5e39fcaa2
Emilname <emil.m.m@mail.ru>
2023-11-04 10:58:01 +0300

    +: Todo class, Sorter class

--- script.js
+++ script.js
@@ -1,83 +1,101 @@
-/*
-  1. Todo list с массивом даных todos: []
-  2. Реализуем параметр done у объекта todo
-  3. Удаляем todo через метод filter
-  4. Добавим время выполнения задачи
-  5. Сортировка asc, desc
-  6. Общее время открытых задач с помощью reduce
-  7. Объекты keys, values, entries
-  8. Прототипы
-  9. this и методы объектов
-  10. Переписываем todolist на классы
-*/
+const $ = (selector) => document.querySelector(selector);
 
-const sortByDoneButton = document.querySelector(".todo__sort-button.done");
-const sortByTimeButton = document.querySelector(".todo__sort-button.time");
-const todoTextInput = document.querySelector(".todo__text-input");
-const todoTimeInput = document.querySelector(".todo__time-input");
-const todoList = document.querySelector(".todo__list");
-const todoForm = document.querySelector(".todo__form");
-const timeValue = document.querySelector(".todo__time-value");
+const sortByDoneButton = $(".todo__sort-button.done");
+const sortByTimeButton = $(".todo__sort-button.time");
+const todoTextInput = $(".todo__text-input");
+const todoTimeInput = $(".todo__time-input");
+const todoList = $(".todo__list");
+const todoForm = $(".todo__form");
+const timeValue = $(".todo__time-value");
 
-const SORT_TYPES = {
-  asc: "asc",
-  desc: "desc",
+const sortOrders = {
+  ASC: "asc",
+  DESC: "desc",
 };
 
-let todos = [];
-let sortType = null;
+Object.freeze(sortOrders);
 
-function sortByKey(key) {
-  if (!sortType || sortType === SORT_TYPES.desc) {
-    todos.sort(function (a, b) {
-      return Number(a[key]) - Number(b[key]);
-    });
+class Sorter {
+  sortedFieldKey = null;
+  sortOrder = sortOrders.ASC;
 
-    sortType = SORT_TYPES.asc;
-  } else {
-    todos.sort(function (a, b) {
-      return Number(b[key]) - Number(a[key]);
-    });
-
-    sortType = SORT_TYPES.desc;
+  constructor() {
+    this.setDefaultSortOrder();
   }
 
-  renderTodos();
+  setDefaultSortOrder = () => {
+    this.sortOrder = sortOrders.ASC;
+  };
+
+  sortAsc = (a, b) => {
+    return Number(b[this.sortedFieldKey]) - Number(a[this.sortedFieldKey]);
+  };
+  sortDesc = (a, b) => {
+    return Number(a[this.sortedFieldKey]) - Number(b[this.sortedFieldKey]);
+  };
+
+  switchSortOrder = () => {
+    if (this.sortOrder == sortOrders.ASC) {
+      this.sortOrder = sortOrders.DESC;
+    } else {
+      this.sortOrder = sortOrders.ASC;
+    }
+  };
+
+  sortByField = (fieldKey, list) => {
+    if (this.sortedFieldKey == fieldKey) {
+      this.switchSortOrder();
+    } else {
+      this.setDefaultSortOrder();
+    }
+    this.sortedFieldKey = fieldKey;
+    return list.sort(
+      this.sortOrder == sortOrders.ASC ? this.sortAsc : this.sortDesc
+    );
+  };
 }
 
-sortByDoneButton.addEventListener("click", function () {
-  sortByKey("done");
-});
-
-sortByTimeButton.addEventListener("click", function () {
-  sortByKey("time");
-});
-
-class BaseTodo {
-  constructor(text, done) {
-    this.id = Date.now();
+class Todo {
+  constructor({ id, text = "", done = false, time = "0" }) {
+    this.id = id ?? Date.now();
     this.text = text;
     this.done = done;
-  }
-
-  logText() {
-    console.log(this.text);
-    return 0;
-  }
-}
-
-class TodoWithDeadline extends BaseTodo {
-  constructor(text, done, time) {
-    super(text, done);
-
     this.time = time;
+  }
+
+  render() {
+    const liElement = document.createElement("li");
+
+    liElement.id = this.id;
+    liElement.classList.add("todo__item");
+
+    if (this.done) {
+      liElement.classList.add("done");
+    }
+
+    const todoTemplate = `
+        <span class="todo__item-text">${this.text}</span>
+        ${this.time ? `<span class="todo__item-time">${this.time}</span>` : ""}
+        <div class="todo__controls">
+          <button class="todo__done-button">Выполнено</button>
+          <button class="todo__remove-button">Удалить</button>
+        </div>
+      `;
+
+    liElement.innerHTML = todoTemplate;
+
+    return liElement;
+  }
+
+  toString() {
+    return JSON.stringify({ ...this });
   }
 }
 
 class LStorage {
   getTodos() {
     const todos = localStorage.getItem("todos");
-    const parsedTodos = JSON.parse(todos);
+    const parsedTodos = JSON.parse(todos).map((todo) => new Todo(todo));
 
     return parsedTodos;
   }
@@ -93,23 +111,44 @@ class TodoList {
   constructor(storage) {
     this.storage = storage;
     this.todos = this.storage.getTodos() || [];
+    this.sorter = new Sorter();
   }
 
   init = () => {
     this.render();
+    this.addSubmitListener();
+    this.addDoneSortListener();
+    this.addTimeSortListener();
+  };
 
+  addDoneSortListener = () => {
+    sortByDoneButton.addEventListener("click", () => {
+      this.sortByField("done");
+    });
+  };
+
+  addTimeSortListener = () => {
+    sortByTimeButton.addEventListener("click", () => {
+      this.sortByField("time");
+    });
+  };
+
+  sortByField = (key) => {
+    this.todos = this.sorter.sortByField(key, this.todos);
+    this.render();
+  };
+
+  addSubmitListener = () => {
     todoForm.addEventListener("submit", (event) => {
       event.preventDefault();
       const todoText = todoTextInput.value;
       const todoTime = todoTimeInput.value;
       const trimmedText = todoText.trim();
 
-      const todo = {
-        id: Date.now(),
+      const todo = new Todo({
         text: trimmedText,
-        done: false,
         time: todoTime,
-      };
+      });
 
       if (trimmedText) {
         this.todos.push(todo);
@@ -124,21 +163,26 @@ class TodoList {
   };
 
   render = () => {
-    const todosNodes = this.todos.map(this.createTodo);
-
     todoList.innerHTML = "";
 
-    todosNodes.forEach(function (todoNode) {
-      todoList.appendChild(todoNode);
+    this.todos.forEach((todo) => {
+      const todoElement = todo.render();
+      todoElement.addEventListener("click", this.todoItemListener);
+      todoList.appendChild(todoElement);
     });
 
-    const timeSumNonCompletedTodos = todos.reduce(function (acc, curr, idx) {
+    const timeSumNonCompletedTodos = this.todos.reduce(function (
+      acc,
+      curr,
+      idx
+    ) {
       if (!curr.done) {
         acc += Number(curr.time);
       }
 
       return acc;
-    }, 0);
+    },
+    0);
 
     this.storage.setTodos(this.todos);
 
@@ -167,32 +211,6 @@ class TodoList {
 
       this.render();
     }
-  };
-
-  createTodo = (todo) => {
-    const liElement = document.createElement("li");
-
-    liElement.id = todo.id;
-    liElement.classList.add("todo__item");
-
-    if (todo.done) {
-      liElement.classList.add("done");
-    }
-
-    const todoTemplate = `
-      <span class="todo__item-text">${todo.text}</span>
-      ${todo.time ? `<span class="todo__item-time">${todo.time}</span>` : ""}
-      <div class="todo__controls">
-        <button class="todo__done-button">Выполнено</button>
-        <button class="todo__remove-button">Удалить</button>
-      </div>
-    `;
-
-    liElement.innerHTML = todoTemplate;
-
-    liElement.addEventListener("click", this.todoItemListener);
-
-    return liElement;
   };
 }
 
